ae9fa26a7e2b21061cbbd8e17960b6129301db28
Christymonthomas <christymonthomas1999@gmail.com>
2024-07-18 20:17:28 +0530

    Added functionality of todo listing in ui

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React, { Fragment } from "react";
 import "./App.css";
 //Components
 import InputTodo from "./components/InputTodo";
-
+import ListTodo from "./components/ListTodo";
 const App = () => {
   return (
     <Fragment>
       <div className="container">
         <InputTodo />
+        <ListTodo />
       </div>
     </Fragment>
   );
